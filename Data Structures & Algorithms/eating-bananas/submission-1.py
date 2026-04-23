c11670ad2c90c2cdf86456b7aac7d6e2eff2c25a
neelamnee <139762148+neelamnee@users.noreply.github.com>
2026-04-23 20:33:40 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minEatingSpeed(self, piles, h):
+        l, r = 1, max(piles)
+        
+        while l < r:
+            m = (l + r) // 2
+            hours = 0
+            
+            for p in piles:
+                hours += (p + m - 1) // m   # ceil without math
+            
+            if hours > h:
+                l = m + 1
+            else:
+                r = m
+        
+        return l
